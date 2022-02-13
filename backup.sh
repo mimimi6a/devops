65732499f701cb2e97061b089dcc28219deb8450
volha.korziuk <volha.korziuk@gmail.com>
2022-02-13 10:59:44 -0500

    Update script and add test files

--- backup.sh
+++ backup.sh
@@ -6,8 +6,23 @@ BACKUPDIRECTORY=$1
 #compression algorithm
 COMPRESSIONALGORITHM=$2
 
+case $COMPRESSIONALALGORITHM in
+	gzip )
+		COMPRESSION="cvzf"
+		;;
+	bzip )
+		COMPRESSION="cvjf"
+		;;
+	xz ) 
+		COMPRESSION="cvjf"
+		;;
+	none )
+		COMPRESSION="cvf"
+		;;
+esac
+	
 #outpit file name
 OUTPUTFILE=$3
 
 #directory comprehension and encription of the results
-tar $COMPRESSIONALGORITHM $OUTPUTFILE $BACKUPDIRECTORY | openssl enc -e -aes-256-cbc -out $OUTPUTFILE.enc 2> error.log
+tar -cvf $COMPRESSION $OUTPUTFILE $BACKUPDIRECTORY | openssl enc -e -aes-256-cbc -out $OUTPUTFILE.enc > /dev/null  2> error.log
